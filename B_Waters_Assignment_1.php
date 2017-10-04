cd4ba558c6ad9b23902b26316d0a1c6f4329dda1
Ben Waters <bjwaters@email.neit.edu>
2017-10-04 14:08:48 -0400

    Probably done

--- B_Waters_Assignment_1.php
+++ B_Waters_Assignment_1.php
@@ -1,16 +1,25 @@
 <?php
+/*Creating a table start*/
 $table = "<table> <tbody>";
+
+/*The loop for the table creation, first rows and then columns*/
 for($rows = 0; $rows <=9; $rows++)
 {
     $table .= "<tr>";
     for($cols = 0; $cols <= 9; $cols++)
     {
-        $table .= "<td>" . $rows * $cols . "</td>";
+        /*Generating the random numbers*/
+        $number = mt_rand(1, 255);
+        $number2 = mt_rand(1, 255);
+        $number3 = mt_rand(1, 255);
+        /*Inserting the random numbers into the styling of the table*/
+        $table .= "<td style='background-color:rgb($number, $number2, $number3);'> " . dechex($number) . dechex($number2) . dechex($number3). "<br />";
+        $table .= "<span style = 'color: #ffffff;'>" . dechex($number) . dechex($number2) . dechex($number3) . "</span></td>";
     }
     $table .= "</tr>";
 }
-
-$table += "</tbody></table>";
+/*ending the table*/
+$table .= "</tbody></table>";
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +29,7 @@ $table += "</tbody></table>";
     <title>Title</title>
 </head>
 <body>
+<!-- Php goes here -->
 <?php echo "$table"; ?>
 </body>
 </html>
