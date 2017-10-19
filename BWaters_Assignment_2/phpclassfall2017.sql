5c6ecb63a1a8085833c89d7cd015ef35eebfc72d
Ben Waters <bjwaters@email.neit.edu>
2017-10-19 15:05:42 -0400

    Added database file

---
+++ BWaters_Assignment_2/phpclassfall2017.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2017 at 09:02 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpclassfall2017`
+--
+CREATE DATABASE IF NOT EXISTS `phpclassfall2017` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `phpclassfall2017`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actors`
+--
+
+DROP TABLE IF EXISTS `actors`;
+CREATE TABLE `actors` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `firstname` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `dob` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `height` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `actors`
+--
+
+INSERT INTO `actors` (`id`, `firstname`, `lastname`, `dob`, `height`) VALUES
+(1, 'Arnold', 'Schwarzenegger ', '6&#39; 2&#34;', ''),
+(2, 'Jean-Claude ', 'Van Damme ', '5&#39; 10&#34;', ''),
+(3, 'Maria', 'Bravo', '4&#39;7&#34;', ''),
+(4, 'Freddy', 'Highmoe', '5&#39;10&#34;', ''),
+(5, 'Robin', 'Williams', '5&#39;7&#34;', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `actors`
+--
+ALTER TABLE `actors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `actors`
+--
+ALTER TABLE `actors`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
